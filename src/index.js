00b4c14d46341a4fb36fc6ecc0e9b0e2d7b27f12
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-05-25 16:56:54 +0530

    my first react component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+      <h2>This is Pizza Spinaci</h2>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
